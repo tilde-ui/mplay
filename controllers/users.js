b6dedda935565ba2d2aeba8feb4790b0ae7fcc3f
Akshat Pandey <akshat0123@gmail.com>
2015-11-13 21:45:51 -0500

    users can now be registered

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,30 @@
 var express = require('express')
 var router  = express.Router();
-var model   = require('../models/user'); 
+var user    = require('../models/user'); 
 
 router.post('/register', function(req, res, next) {
 	console.log('USERNAME: ' + req.body.username);
 	console.log('PASSWORD: ' + req.body.password);
 	console.log('CONFIRM: ' + req.body.confirm_pw);
+	var username   = req.body.username;
+	var password   = req.body.password;
+	var confirm_pw = req.body.confirm_pw;
+	user.findOne({ username : username }, function(err, found) {
+		if (err) { throw err; }
+		else if (found) { console.log('Already a user!'); }
+		else { 
+			var newuser = new user({
+				username : username,
+				password : password
+			});
+			newuser.save(function(err) {
+				if (err) { throw err; }
+				else { 
+					console.log('user created. you a mothafukin boss bruh');
+					res.render('login'); }
+			});
+		}
+	});
 });
 
 module.exports = router;
