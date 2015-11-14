b46cbbd47e2073fd252ef79539820d3fbea3223c
Akshat Pandey <akshat0123@gmail.com>
2015-11-14 15:56:01 -0500

    changed user model & register page to match mockups

--- controllers/users.js
+++ controllers/users.js
@@ -3,15 +3,15 @@ var router  = express.Router();
 var user    = require('../models/user'); 
 
 router.post('/login', function(req, res, next) {
-	var username = req.body.username;
+	var email 	 = req.body.email;
 	var password = req.body.password;
-	user.findOne({ username : username }, function(err, found) {
+	user.findOne({ email : email }, function(err, found) {
 		if (err) { throw err; }
 		else if (found) { 
 			if (found.password !== password) {
 				res.render('login', { message: 'Incorrect password' });
 			} else {
-				req.session.uid = username;
+				req.session.uid = email;
 				res.render('login', { message: 'Successful login' }); 
 			}
 		} else {
@@ -21,21 +21,25 @@ router.post('/login', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next) {
-	var username   = req.body.username;
+	var firstName  = req.body.firstName;
+	var lastName	 = req.body.lastName;
+	var email   	 = req.body.email;
 	var password   = req.body.password;
 	var confirm_pw = req.body.confirm_pw;
 	if (password !== confirm_pw) { 
 		res.render('register', { message: 'Passwords don\'t match!' }); 
 	} else {
-		user.findOne({ username : username }, function(err, found) {
+		user.findOne({ email : email }, function(err, found) {
 			if (err) { throw err; }
 			else if (found) { 
 				res.render('register', { message: 'Username exists!' }); 
 			}
 			else { 
 				var newuser = new user({
-					username : username,
-					password : password
+					firstName : firstName,
+					lastName  : lastName,
+					email 		: email,
+					password  : password
 				});
 				newuser.save(function(err) {
 					if (err) { throw err; }
--- models/user.js
+++ models/user.js
@@ -1,9 +1,17 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/mplay_db');
 
+// var userSchema = new mongoose.Schema({
+// 	username: { type: String, required: true, unique: true }, 
+// 	password: { type: String, required: true}, 
+// });
+
 var userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true }, 
-	password: { type: String, required: true}, 
+	firstName : { type: String, required: true },
+	lastName  : { type: String, required: true },
+	email  		: { type: String, required: true, unique: true }, 
+	password  : { type: String, required: true}, 
+	bio				: { type: String, required: false}
 });
 
 var user = mongoose.model('user', userSchema);
