211106dd2c4bd743b16051be8840942642d1bc83
Sam Nelson <sam@1x-vl943-72-19-92-191.wireless.umass.edu>
2015-11-20 12:56:08 -0500

    Removed Username and password from user schema
    non necessary and redundant fields

--- models/user.js
+++ models/user.js
@@ -4,8 +4,7 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var userSchema = new mongoose.Schema({
 	firstName : { type: String, required: true },
 	lastName  : { type: String, required: true },
-	username  : { type: String, required: true, unique: true }, 
-	password  : { type: String, required: true}, 
+	//username is automatically added by pasport
 	bio				: { type: String, required: false}
 });
 
