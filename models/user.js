71fe871c3cc605e76e8a38aeb39d9a4c7a3c8d30
Sam Nelson <sam@1x-vl943-72-19-92-191.wireless.umass.edu>
2015-11-20 13:23:53 -0500

    added email to register view and model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ var userSchema = new mongoose.Schema({
 	firstName : { type: String, required: true },
 	lastName  : { type: String, required: true },
 	//username is automatically added by pasport
-	bio				: { type: String, required: false}
+	bio				: { type: String, required: false},
+	email			:	{ type: String, lowercase: true, trim: true}
 });
 
 userSchema.plugin(passportLocalMongoose);
