854f26ee057325461d251fd7b46aa43ceb71ff9e
Ryan Herlihy <ryan.herlihy93@gmail.com>
2015-12-05 23:51:35 -0500

    added scripts to layout fixed settings buttons

--- controllers/index.js
+++ controllers/index.js
@@ -9,31 +9,31 @@ router.get('/', (req, res, next) => {
 
 router.get('/dash', (req, res, next) => {
 	if (!req.user) { res.render('login', { message : 'You must login first!', isAuth : req.isAuthenticated() }); }
-  else 					 { res.render('login', { isAuth : req.isAuthenticated() 																	 }); }
+  else 					 { res.render('dashboard', { isAuth : req.isAuthenticated() 																	 }); }
 });
 
-router.get('/organization', (req, res, next) => { 
+router.get('/organization', (req, res, next) => {
 	if (!req.user) { res.render('login', 				{ message: 'You must login first!', isAuth : req.isAuthenticated() }); }
 	else 					 { res.render('organization', { isAuth : req.isAuthenticated() 																	 }); }
 });
 
-router.get('/settings', (req, res, next) => { 
+router.get('/settings', (req, res, next) => {
 	if (!req.user) { res.render('login', 		{ message : 'You must login first!', isAuth : req.isAuthenticated() }); }
 	else 					 { res.render('settings', { user : req.user, 									 isAuth : req.isAuthenticated() }); }
 });
 
-router.get('/installation', (req, res, next) => { 
-	if (!req.user) { res.render('login', 		{ message : 'You must login first!', isAuth : req.isAuthenticated }); } 
+router.get('/installation', (req, res, next) => {
+	if (!req.user) { res.render('login', 		{ message : 'You must login first!', isAuth : req.isAuthenticated }); }
 	else 					 { res.render('installation', { isAuth : req.isAuthenticated() }); }
 });
-router.get('/playlists', (req, res, next) => { 
+router.get('/playlists', (req, res, next) => {
 	if (!req.user) { res.render('login', 			 { message : 'You must login first!', isAuth : req.isAuthenticated }); }
 	else 					 { res.render('playlists', 	 { isAuth : req.isAuthenticated() }); }
 });
 
 router.get('/login', (req, res, next) => { res.render('login', { isAuth : req.isAuthenticated() }); });
 
-router.get('/register', (req, res, next) => { 
+router.get('/register', (req, res, next) => {
 	organization.find((err, orgs) => {
 		res.render('register', { orgs : orgs, isAuth : req.isAuthenticated() });
 	});
