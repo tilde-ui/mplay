7f466f692368063a43571ba18d7164a5c16de0e2
Ryan Herlihy <ryan.herlihy93@gmail.com>
2015-12-10 13:22:37 -0500

    organization user added

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -7,11 +7,11 @@ router.post('/register', (req, res, next) => {
 		if (found) { res.render('organization', { message : 'Organization exists', isAuth: req.isAuthenticated() }); }
 		else {
 			new organization({
-				name 				: req.body.name, 
+				name 				: req.body.name,
 				description : req.body.description
 			}).save((err, org) => {
 				if (err) { res.render('organization', { message : 'Error', 										isAuth : req.isAuthenticated() }); }
-				else 		 { res.render('dashboard', 		{ message : 'Organization registered!', isAuth : req.isAuthenticated() }); } 
+				else 		 { res.render('dashboard', 		{ message : 'Organization registered!', user : req.user, isAuth : req.isAuthenticated() }); } 
 			});
 		}
 	});
